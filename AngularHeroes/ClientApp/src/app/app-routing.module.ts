cafd8ec886111b8b2c05d0b269e8dc40ba84bb68
nvv <n.vanveen@control-it.nl>
2020-05-23 09:40:08 +0200

    Integrated active-directory-javascript-singlepageapp-angular-master application into Heroes

--- AngularHeroes/ClientApp/src/app/app-routing.module.ts
+++ AngularHeroes/ClientApp/src/app/app-routing.module.ts
@@ -1,22 +1,28 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
 
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [MsalGuard] }
+
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
 
 
 /*
--- AngularHeroes/ClientApp/src/app/app.module.ts
+++ AngularHeroes/ClientApp/src/app/app.module.ts
@@ -1,40 +1,75 @@
 import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule }    from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
-
+import { HTTP_INTERCEPTORS, HttpClientModule }    from '@angular/common/http';
+import { MatToolbarModule, MatButtonModule, MatListModule } from '@angular/material';
+import { MsalModule, MsalInterceptor } from '@azure/msal-angular';
 
 import { AppRoutingModule }     from './app-routing.module';
+import { AppComponent } from './app.component';
 
-
-import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroSearchComponent }  from './hero-search/hero-search.component';
 import { MessagesComponent }    from './messages/messages.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    //HttpClientInMemoryWebApiModule.forRoot(
-    //  InMemoryDataService, { dataEncapsulation: false }
-    //)
-  ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    ProfileComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule,
+    MsalModule.forRoot({
+      auth: {
+        clientId: '4d13a0f9-eeff-4a4f-9f05-4d13a0f94a4f', // fake
+        authority: 'https://login.microsoftonline.com/af5bff61-3f3a-94a4f-9015-4d13a0f994a4f', // fake
+        redirectUri: 'http://localhost:4200/',
+      },
+      cache: {
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: isIE, // set to true for IE 11
+      },
+    },
+      {
+        popUp: !isIE,
+        consentScopes: [
+          'user.read',
+          'openid',
+          'profile',
+        ],
+        unprotectedResources: [],
+        protectedResourceMap: [
+          ['https://graph.microsoft.com', ['user.read']]
+        ],
+        extraQueryParameters: {}
+      })
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [ AppComponent ]
 })
